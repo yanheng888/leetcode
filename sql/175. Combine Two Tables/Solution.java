6d926392254ac7f72e533db9bc2689666ede4179
yanheng888 <70694931+yanheng888@users.noreply.github.com>
2022-03-04 14:02:54 +0800

    Update Solution.java

--- sql/175. Combine Two Tables/Solution.java
+++ sql/175. Combine Two Tables/Solution.java
@@ -2,8 +2,3 @@ select FirstName, LastName, City, State
 from Person left join Address
 on Person.PersonId = Address.PersonId
 ;
-
-作者：LeetCode
-链接：https://leetcode-cn.com/problems/combine-two-tables/solution/zu-he-liang-ge-biao-by-leetcode/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
