aab301530d90d6fec0dbf6c1633515a6a93e5d7c
yanheng888 <70694931+yanheng888@users.noreply.github.com>
2022-04-13 11:23:11 +0800

    Create Solution.java

---
+++ 二分法/35. Search Insert Position/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int low = 0, high = nums.length-1;
+        while (low <= high){
+            int mid = (high-low)/2+low;
+            if(nums[mid] < target){
+                high = mid-1;
+            }else if(nums[mid] == target){
+                return mid;
+            }else{
+                low = mid+1;
+            }
+        }
+        return high;
+    }
+}
