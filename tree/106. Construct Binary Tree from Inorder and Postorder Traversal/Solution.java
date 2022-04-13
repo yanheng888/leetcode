429ea5b0b24d905cbb66757ebf34abbf1796f273
yanheng888 <70694931+yanheng888@users.noreply.github.com>
2022-04-13 20:48:50 +0800

    Update Solution.java

--- tree/106. Construct Binary Tree from Inorder and Postorder Traversal/Solution.java
+++ tree/106. Construct Binary Tree from Inorder and Postorder Traversal/Solution.java
@@ -16,26 +16,31 @@ import java.util.HashMap;
  * }
  */
 class Solution {
-    HashMap<Integer, Integer> map = new HashMap<>();
+    HashMap<Integer,Integer> map;
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-        if(postorder.length == 0){
-            return null;
+        map = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i],i);
         }
-        map.clear();
-        for(int i = 0; i < inorder.length; i++){
-            map.put(inorder[i], i);
-        }
-        return dfs(postorder, 0, postorder.length - 1, inorder, 0, inorder.length - 1);
+        return dfs(inorder,0,postorder.length-1,postorder,0,postorder.length-1);
     }
 
-    private TreeNode dfs(int[] postorder, int a, int b, int[] inorder, int m, int n) {
-        if(a > b){
+    private TreeNode dfs(int[] inorder, int low1, int high1, int[] postorder, int low2, int high2) {
+        int n = inorder.length;
+        if(n == 0){
             return null;
         }
-        TreeNode root = new TreeNode(postorder[b]);
-        int pos = map.get(postorder[b]);
-        root.left = dfs(postorder, a, a + pos - m - 1, inorder, m, pos - 1);
-        root.right = dfs(postorder, a+pos-m, b-1, inorder, pos+1, n);
+        if(n == 1){
+            return new TreeNode(inorder[0]);
+        }
+        if(low1 > high1 || low2 > high2){
+            return null;
+        }
+        TreeNode root = new TreeNode(postorder[high2]);
+        int rootPosition = map.get(root.val);
+        int leftLen = rootPosition-1-low1+1;
+        root.left = dfs(inorder,low1,rootPosition-1,postorder,low2,low2+leftLen-1);
+        root.right = dfs(inorder,rootPosition+1,high1,postorder,low2+leftLen,high2-1);
         return root;
     }
 }
