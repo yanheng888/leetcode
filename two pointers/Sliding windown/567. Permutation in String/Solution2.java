89a0fed3e3bcedf949d3de8249d8e6984c845de7
yanheng888 <70694931+yanheng888@users.noreply.github.com>
2022-04-18 00:56:50 +0800

    Update Solution2.java

--- two pointers/Sliding windown/567. Permutation in String/Solution2.java
+++ two pointers/Sliding windown/567. Permutation in String/Solution2.java
@@ -1,26 +1,28 @@
+import java.util.Arrays;
+
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
+        int[] cnt1 = new int[26];
+        int[] cnt2 = new int[26];
         int n = s1.length(), m = s2.length();
-        if (n > m) {
+        if(n>m){
             return false;
         }
-        int[] cnt = new int[26];
-        for (int i = 0; i < n; ++i) {
-            --cnt[s1.charAt(i) - 'a'];
+
+        for (int i = 0; i < n; i++) {
+            cnt1[s1.charAt(i)-'a']++;
+            cnt2[s2.charAt(i)-'a']++;
         }
-        int left = 0;
-        for (int right = 0; right < m; ++right) {
-            int x = s2.charAt(right) - 'a';
-            ++cnt[x];
-            while (cnt[x] > 0) {
-                --cnt[s2.charAt(left) - 'a'];
-                ++left;
-            }
-            if (right - left + 1 == n) {
+        if(Arrays.equals(cnt1,cnt2)){
+                return true;
+        }
+        for (int i = n; i < m; i++) {
+            cnt2[s2.charAt(i)-'a']++;
+            cnt2[s2.charAt(i-n)-'a']--;
+            if(Arrays.equals(cnt1,cnt2)){
                 return true;
             }
         }
         return false;
     }
 }
-
