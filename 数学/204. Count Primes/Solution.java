e78c372740471a62d7be36ff9c7eede07b50d772
yanheng888 <70694931+yanheng888@users.noreply.github.com>
2022-04-18 23:47:54 +0800

    Create Solution.java

---
+++ 数学/204. Count Primes/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int countPrimes(int n) {
+        boolean[] isPrime = new boolean[n+1];
+        for(int i=2;i<n;i++){
+            isPrime[i] = true;
+        }
+        for(int i=2;i*i<n;i++){
+            if(!isPrime[i]){
+                continue;
+            }
+            for(int j=i*i;j<n;j+=i){
+                isPrime[j] = false;
+            }
+        }
+        int res=0;
+        for(int i=2;i<n;i++){
+            if(isPrime[i]){
+                res++;
+            }
+        }
+        return res;
+    }
+}
