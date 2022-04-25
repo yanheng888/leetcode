598078fea58a4bded7d540201b6ca42915119009
yanheng888 <70694931+yanheng888@users.noreply.github.com>
2022-04-25 22:43:47 +0800

    Update Solution.java

--- 二分法/4. Median of Two Sorted Arrays/Solution.java
+++ 二分法/4. Median of Two Sorted Arrays/Solution.java
@@ -1,6 +1,7 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int n1 = nums1.length, n2 = nums2.length;
+        //nums1's length will always shorter than nums2's length
         if(n1 > n2){
             return findMedianSortedArrays(nums2, nums1);
         }
@@ -8,11 +9,14 @@ class Solution {
         int left = 0, right = n1;
         while (left <= right){
             int pivx = (left+right)/2;
+            //pivy is determined by pivx, pivx decrease cause pivy increase
             int pivy = (n1+n2+1)/2 - pivx;
 
+            //pivx=0 means there is no element in LX
             int maxLX = pivx == 0 ? Integer.MIN_VALUE : nums1[pivx-1];
             int maxLY = pivy == 0 ? Integer.MIN_VALUE : nums2[pivy-1];
 
+            //pivx=n1 means there is no element in RX
             int minRX = pivx == n1 ? Integer.MAX_VALUE : nums1[pivx];
             int minRY = pivy == n2 ? Integer.MAX_VALUE : nums2[pivy];
 
@@ -22,7 +26,7 @@ class Solution {
                 }else{
                     return Math.max(maxLX,maxLY)/1.0;
                 }
-            }else if(maxLX < minRY){
+            }else if(maxLX <= minRY){
                 left = pivx + 1;
             }else{
                 right = pivx - 1;
