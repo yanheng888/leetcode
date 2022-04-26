b1dd07934deeb9d3310b56129d153e49d019d0b5
yanheng888 <70694931+yanheng888@users.noreply.github.com>
2022-04-26 17:08:16 +0800

    Update Solution.java

--- 二分法/81. Search in Rotated Sorted Array II/Solution.java
+++ 二分法/81. Search in Rotated Sorted Array II/Solution.java
@@ -1,47 +1,36 @@
 class Solution {
     public boolean search(int[] nums, int target) {
         int n = nums.length;
-        if(n < 8){
-            for (int num: nums){
-                if(num == target){
-                    return true;
-                }
-            }
-            return false;
+        int l=0,r=n-1;
+        while(l < r && nums[0] == nums[r]){
+            r--;
         }
-        int low = 0, high = n-1;
-        while(low < high && nums[0] == nums[high]){
-            high--;
-        }
-        while (low <= high && nums[low] >= nums[n-1]){
-            int mid = low+(high-low)/2;
-            if(nums[mid]>=nums[low]){
-                low = mid+1;
+        while (l<r && nums[l] > nums[r]){
+            int m = l+(r-l)/2;
+            if(nums[m] > nums[r]){
+                l = m+1;
             }else{
-                high = mid;
+                r = m;
             }
         }
-
         if(target == nums[n-1]){
             return true;
-        }
-        if(target>nums[n-1]){
-            if(low >= 1 && low < n && nums[low] < nums[low-1]){
-                low--;
+        }else if(target > nums[n-1]){
+            if(l>=1 && nums[l] < nums[l-1]){
+                r = l-1;
             }
-            high = low;
-            low = 0;
+            l = 0;
         }else{
-            high = n-1;
+            r = n-1;
         }
-        while (low <= high){
-            int mid = low+(high-low+1)/2;
-            if(nums[mid]<target){
-                low = mid+1;
-            }else if(nums[mid]>target){
-                high = mid-1;
-            }else{
+        while (l<=r){
+            int m = l+(r-l)/2;
+            if(nums[m] == target){
                 return true;
+            }else if(nums[m] < target){
+                l = m+1;
+            }else{
+                r = m-1;
             }
         }
         return false;
